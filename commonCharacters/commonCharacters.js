2de1a4711ba326abdce3d252b8c235ad47caad6d
guizenifarouk <guizeni.farouk95@gmail.com>
2019-11-27 09:37:20 +0100

    toy

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,15 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+    var result ='';
+  string1 = string1.toLowerCase()
+  string2 = string2.toLowerCase()
+  for(var i = 0 ; i < string1.length ; i++) {
+  	for(var j = 0 ; j < string2.length; j++) {
+  		if(string1[i] === string2[j]) {
+  			result += string2[j]
+  		}
+  	}
+  }
+  return result;
 };
