3703d0db8ffd76b08578b023b97a0dd888977275
guizenifarouk <guizeni.farouk95@gmail.com>
2019-12-06 09:53:09 +0100

    unfinished

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -11,3 +11,32 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+function vowelsBack(s) {
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	var consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+	var vowels = ['a','e','i','o','u'];
+	var r = s
+for(var x = 0 ; i < r.length ; x++) {
+	if(r[x]==='c' || r[x]=== 'o') {
+		r[x]= alphabet[alphabet.indexOf(r[x])-1]
+	}
+	if(r[x]==='d') {
+		r[x]= alphabet[alphabet.indexOf(r[x])-3]
+	}
+	if(r[x]==='e') {
+		r[x]= alphabet[alphabet.indexOf(r[x])-4]
+	}
+	for(var i = 0 ; i < consonants.length ; i++) {
+		if(r[x] === consonants[i]) {
+			r[x]= alphabet[alphabet.indexOf(r[x])+9]
+		}
+	}
+	for(var j = 0 ; j < vowels.length ; j++) {
+		if(r[x]=== vowels[j]) {
+			r[x]= alphabet[alphabet.indexOf(r[x])-5]
+		}
+	}
+}
+return r
+
+}
