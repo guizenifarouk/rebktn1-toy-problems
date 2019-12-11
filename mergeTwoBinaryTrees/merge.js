9c9c926d349a0f8096c8339533174021f166278f
guizenifarouk <guizeni.farouk95@gmail.com>
2019-12-11 09:44:49 +0100

    mergetrees

--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -36,4 +36,18 @@ Note: The merging process must start from the root nodes of both trees.
  * @param {TreeNode} t2
  * @return {TreeNode}
  */
-var mergeTrees = function(t1, t2) {};
+
+var mergeTrees = function(t1, t2) {
+	if(!t1){
+		return t2
+	}
+	if(!t2) {
+		return t1
+	}
+	if(t1 && t2) {
+		t1.value += t2.value
+	}
+	t1.left =  mergeTrees(t1.left, t2.left)
+	t1.right = mergeTrees(t1.right, t2.right) 
+
+};
