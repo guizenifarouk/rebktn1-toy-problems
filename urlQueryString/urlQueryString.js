f321caffe57d1482e48e3ac75493a3a75a9f6278
guizenifarouk <guizeni.farouk95@gmail.com>
2019-12-13 09:45:50 +0100

    urlQuery

--- urlQueryString/urlQueryString.js
+++ urlQueryString/urlQueryString.js
@@ -9,4 +9,20 @@ parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello wo
 parseQueryString("http://example.com") // undefined
 */
 
-function parseQueryString(url) {}
+function parseQueryString(url) {
+	var array = url.split('')
+	var index1 = array.indexOf('?')
+	var arr = array.splice(index1)
+
+	var index2 = arr.indexOf('=')
+	var index3 = arr.indexOf('&')
+	
+	// console.log(arr.slice(1,index2).join(''))
+	// console.log(arr.slice(index2+1,index3).join(''))
+
+	var partOne = [arr.slice(1, index2).join(''),arr.slice(index2+1, index3).join('')]
+	var arr1 = arr.splice(arr.indexOf('&'))
+	var partTwo =[arr1.slice(1, arr1.indexOf('=')).join(''),arr1.slice(arr1.indexOf('=')+1).join('')]
+
+	return [partOne, partTwo]
+}
